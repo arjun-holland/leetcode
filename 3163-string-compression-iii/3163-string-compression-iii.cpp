716e4c3c376d66a8826f8462825c8ed572563daf
Mallikarjuna <spiderman05116@gmail.com>
2024-11-04 19:35:10 +0530

    Time: 57 ms (23.93%), Space: 30 MB (9.87%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        int s = word.length();
+        string re = "";
+        int i=0;
+        
+        while(i<s){
+            int c = 0;
+            char ch = word[i];
+            while(i<s && c<9 && word[i]==ch){
+                i++;
+                c++;
+            }
+            re += to_string(c)+ch;
+        }
+        return re;
+    }
+};
