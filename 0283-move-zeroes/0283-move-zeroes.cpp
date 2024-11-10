7e954c367cf09e000e05e29e1360a13698a0f844
Mallikarjuna <spiderman05116@gmail.com>
2024-11-10 21:10:45 +0530

    Time: 0 ms (100.00%), Space: 22 MB (10.33%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> dn;
+      /*  vector<int> dn;
         for(int &i:nums){
             if(i != 0)
                 dn.push_back(i);
@@ -13,6 +13,17 @@ public:
         }
         for(int i=0;i<nums.size();i++){
             nums[i] = dn[i];
+        }
+      */ 
+        int j = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != 0){
+               nums[j] = nums[i];
+               j++;
+            }
+        }
+        for(int i=j; i<nums.size(); i++){
+            nums[i] = 0;
         }
     }
 };
