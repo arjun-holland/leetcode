5115446a438d4527cb357c1d8632cd8905d9c1be
Mallikarjuna <spiderman05116@gmail.com>
2024-11-14 19:23:39 +0530

    Time: 26 ms (87.27%), Space: 87.3 MB (34.13%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // Function to check if it's possible to distribute quantities with a maximum of 'x' per shop
+    bool possibleToDis(int x, vector<int>& quantities, int shops){
+        for(int &q : quantities){
+            // Calculate how many shops are needed for the current quantity with a max capacity of 'x'
+            shops -= (q+x-1)/x;    //ceil(q/x);
+            if(shops < 0)          // If we run out of shops, return false
+                return false;
+        }
+        return  true;
+    }
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int l = 1, r = *max_element(quantities.begin(),quantities.end());
+        int res = 0;
+        
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(possibleToDis(m,quantities,n)){
+                res = m;
+                r = m-1;      // Try for a smaller maximum
+            }else{
+                l = m+1;      // Increase the minimum limit
+            }
+        }
+        return res;
+    }
+};
