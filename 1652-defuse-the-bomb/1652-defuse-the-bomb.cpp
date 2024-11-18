0d1f287a233f2416c5fcb98ab9957dc851882aa8
Mallikarjuna <spiderman05116@gmail.com>
2024-11-18 11:39:17 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (60.07%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    //As the array is circular, use modulo to find the correct index.
+    vector<int> decrypt(vector<int>& code, int k) {
+         vector<int> result(code.size(), 0);
+        if (k == 0) {
+            return result;
+        }
+        for (int i = 0; i < result.size(); i++) {
+            if (k > 0) {
+                for (int j = i + 1; j < i + k + 1; j++) {
+                    result[i] += code[j % code.size()];
+                }
+            } else {
+                for (int j = i - abs(k); j < i; j++) {
+                    result[i] += code[(j + code.size()) % code.size()];
+                }
+            }
+        }
+        return result;
+    }
+};
