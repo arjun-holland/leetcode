36d62d9baace2084daa81812e41508ea54087fb8
Mallikarjuna <spiderman05116@gmail.com>
2024-12-08 15:46:00 +0530

    Time: 2 ms (77.04%), Space: 17.1 MB (36.43%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool canMakeSubsequence(string s1, string s2) {
-        int j = 0; // Pointer for s2
+        int j = 0; 
+        int i = 0;
         
-        for (char c : s1) {
-            // Check if we have matched all characters of s2
-            if (j < s2.length()) {
-                char nextChar = (c == 'z') ? 'a' : c + 1; // Increment character
-                
-                // Check if current character matches or can be transformed to match s2[j]
-                if (c == s2[j] || nextChar == s2[j]) {
-                    j++; // Move to the next character in s2
-                }
-            }
+        while(i<s1.size() && j<s2.size()){
+            if((s1[i] == s2[j])||
+               (s1[i]+1 == s2[j])||
+               (s1[i]-25 == s2[j])){   //z=>25 : a=>0
+                   j++;
+               }
+            i++;
         }
         
-        // If we've matched all characters in s2, j should equal s2.length()
         return j == s2.length();
     }
 };
