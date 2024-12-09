7cfba82bed485c52af1e5daa8947345465df4295
Mallikarjuna <spiderman05116@gmail.com>
2024-12-09 22:27:51 +0530

    Time: 84 ms (56.47%), Space: 133 MB (66.45%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
-    int maxTwoEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end());
-        vector<vector<int>> dp(events.size(), vector<int>(3, -1));
-        return findEvents(events, 0, 0, dp);
+    int n;
+    int t[100001][3];
+    int binarySearch(vector<vector<int>>& events,int ei){
+        int l =0;
+        int r = n-1;
+        int res = n;
+        while(l <= r){
+            int m = l + ( r-l)/2;
+            if(events[m][0] > ei){
+                res = m;
+                r = m - 1;
+            }else{
+                l = m + 1;
+            }
+        }
+        return res;
     }
     
-    int findEvents(vector<vector<int>>& events, int idx, int cnt,
-                   vector<vector<int>>& dp) {
-        if (cnt == 2 || idx >= events.size()) return 0;
-        if (dp[idx][cnt] == -1) {
-            int end = events[idx][1];
-            int lo = idx + 1, hi = events.size() - 1;
-            while (lo < hi) {
-                int mid = lo + ((hi - lo) >> 1);
-                if (events[mid][0] > end)
-                    hi = mid;
-                else
-                    lo = mid + 1;
-            }
-            int include =
-                events[idx][2] + (lo < events.size() && events[lo][0] > end
-                                      ? findEvents(events, lo, cnt + 1, dp)
-                                      : 0);
-            int exclude = findEvents(events, idx + 1, cnt, dp);
-            dp[idx][cnt] = max(include, exclude);
-        }
-        return dp[idx][cnt];
+    int solve(vector<vector<int>>& events,int i,int c){
+        if(c == 2 || i >= n)
+            return 0;
+        if(t[i][c] != -1)
+            return t[i][c];
+        int vin = binarySearch(events,events[i][1]);
+        int take = events[i][2] + solve(events,vin,c+1);
+        
+        int ntake = solve(events,i+1,c);
+        
+        return t[i][c] =  max(take,ntake);
+    }
+    
+    int maxTwoEvents(vector<vector<int>>& events) {
+        n = events.size();
+        memset(t,-1,sizeof(t));
+        sort(events.begin(),events.end());
+        return solve(events,0,0);
     }
 };
